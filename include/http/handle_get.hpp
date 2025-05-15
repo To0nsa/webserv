502d0e81cdbe1addbf27c79c21d648cf1be99db4
Rychkov Iurii <irychkov@student.hive.fi>
2025-05-15 13:20:06 +0300

    Set listing in alphabetical order.

--- include/http/handle_get.hpp
+++ include/http/handle_get.hpp
@@ -6,12 +6,13 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 12:41:46 by irychkov          #+#    #+#             */
-/*   Updated: 2025/05/15 12:47:01 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/05/15 13:06:42 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include <algorithm>
 #include <iostream>
 #include <dirent.h>
 #include <chrono>
--- src/http/handle_get.cpp
+++ src/http/handle_get.cpp
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 12:39:41 by irychkov          #+#    #+#             */
-/*   Updated: 2025/05/15 12:59:12 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/05/15 13:15:48 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,58 +36,100 @@ HttpResponse handleGet(const HttpRequest &request, const Server &server, const L
 		if (loc.isAutoindexEnabled()) {
 			DIR* dir = opendir(filepath.c_str());
 			if (dir == NULL) {
-				std::cout << "[AUTOINDEX] Failed to open directory!" << std::endl;
+				return ResponseBuilder::generateError(403, server, request);
 			}
 			if (dir) {
 				struct dirent* entry;
+				std::vector<std::string> dirs;
+				std::vector<std::string> files;
+
+				// Split entries into folders and files
+				while ((entry = readdir(dir)) != NULL) {
+					std::string name = entry->d_name;
+					if (name == ".")
+						continue;
+					if (name == "..") {
+						dirs.insert(dirs.begin(), name); // Ensure ".." is always first
+						continue;
+					}
+
+					std::string fullPath = filepath + "/" + name;
+					struct stat fileStat;
+					if (stat(fullPath.c_str(), &fileStat) == -1) {
+						perror(("stat failed for " + fullPath).c_str());
+						continue;
+					}
+
+					if (S_ISDIR(fileStat.st_mode))
+						dirs.push_back(name);
+					else
+						files.push_back(name);
+				}
+				closedir(dir);
+
+				// Sort alphabetically
+				if (!dirs.empty() && dirs[0] == "..")
+					std::sort(dirs.begin() + 1, dirs.end()); // Skip ".."
+				else
+					std::sort(dirs.begin(), dirs.end());
+				std::sort(files.begin(), files.end());
+
+				// Start building the HTML
 				std::stringstream indexStream;
 				std::string baseUri = request.getPath();
 				if (baseUri.empty() || baseUri.back() != '/')
 					baseUri += '/';
-				indexStream << "<html><body><h1> &nbsp;&nbsp; Index of " << request.getPath() << "</h1><table><tr><th>Name</th><th>Last Modified (UTC)</th><th>Size (bytes)</th></tr>";
-				while ((entry = readdir(dir)) != NULL) {
-				std::string name = entry->d_name;
-				if (name == ".")
-					continue;
-				if (name == "..") {
-					name += "/";
-					indexStream << "<tr><td style='min-width: 20ch; padding-right: 30px;'><a href='" << baseUri << name << "'>" << name << "</a></td></tr>";
-					continue;
+
+				indexStream << "<html><body><h1> &nbsp;&nbsp; Index of " << request.getPath() << "</h1>";
+				indexStream << "<table><tr><th>Name</th><th>Last Modified (UTC)</th><th>Size (bytes)</th></tr>";
+
+				// Helper lambda to render a row
+				auto renderEntry = [&](const std::string& name) {
+					std::string fullPath = filepath + "/" + name;
+					struct stat fileStat;
+					if (stat(fullPath.c_str(), &fileStat) == -1)
+						return;
+
+					std::stringstream timeStream;
+					auto mod_time = std::chrono::system_clock::from_time_t(fileStat.st_mtime);
+					std::time_t time = std::chrono::system_clock::to_time_t(mod_time);
+					timeStream << std::put_time(std::gmtime(&time), "%d-%b-%Y %H:%M");
+
+					std::string displayName = truncateName(name, 25);
+					std::string href = baseUri + name;
+
+					bool isDir = S_ISDIR(fileStat.st_mode);
+					if (isDir) {
+						displayName += "/";
+						href += "/";
+					}
+
+					indexStream << "<tr>";
+					indexStream << "<td style='min-width: 20ch; padding-right: 30px;'><a href='" << href << "'>" << displayName << "</a></td>";
+					indexStream << "<td style='min-width: 20ch; padding-right: 30px;'>" << timeStream.str() << "</td>";
+					indexStream << "<td style='min-width: 20ch; padding-right: 30px;'>";
+					if (isDir)
+						indexStream << "-";
+					else
+						indexStream << fileStat.st_size;
+					indexStream << "</td></tr>";
+				};
+
+				// Render ".." if present
+				if (!dirs.empty() && dirs[0] == "..") {
+					indexStream << "<tr><td colspan='3'><a href='" << baseUri << "../'>../</a></td></tr>";
+					dirs.erase(dirs.begin());
 				}
 
-				std::string fullPath = filepath + "/" + name;
-				struct stat fileStat;
-				if (stat(fullPath.c_str(), &fileStat) == -1) {
-					perror(("stat failed for " + fullPath).c_str());
-					continue;
-				}
+				// Render sorted directories
+				for (const std::string& dirName : dirs)
+					renderEntry(dirName);
 
-				std::stringstream sizeStream;
-				sizeStream << fileStat.st_size << " bytes";
+				// Render sorted files
+				for (const std::string& fileName : files)
+					renderEntry(fileName);
 
-				auto mod_time = std::chrono::system_clock::from_time_t(fileStat.st_mtime);
-				std::time_t time = std::chrono::system_clock::to_time_t(mod_time);
-				std::stringstream timeStream;
-				timeStream << std::put_time(std::gmtime(&time), "%d-%b-%Y %H:%M");
-
-				std::string displayName = truncateName(name, 25);
-				if (S_ISDIR(fileStat.st_mode)) {
-					displayName += "/";
-				}
-
-				indexStream << "<tr>";
-				indexStream << "<td style='min-width: 20ch; padding-right: 30px;'><a href='" << baseUri << name << "'>" << displayName << "</a></td>";
-				indexStream << "<td style='min-width: 20ch; padding-right: 30px;'>" << timeStream.str() << "</td>";
-				// Only show size for regular files
-				if (S_ISREG(fileStat.st_mode)) {
-					indexStream << "<td style='min-width: 20ch; padding-right: 30px;'>" << fileStat.st_size << "</td>";
-				} else {
-					indexStream << "<td style='min-width: 20ch; padding-right: 30px;'>-</td>";
-				}
-				indexStream << "</tr>";
-			}
 				indexStream << "</table></body></html>";
-				closedir(dir);
 				body = indexStream.str();
 			}
 		}
