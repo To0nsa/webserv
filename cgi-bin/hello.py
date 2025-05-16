ec737443bdf9c48557d93e36bf187b3a49f84ae7
To0nsa <nicolas.lovis@hotmail.fr>
2025-05-16 12:57:29 +0300

    Add script tests and test suite for cgi

--- cgi-bin/hello.py
+++ cgi-bin/hello.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python3
+import os, sys
 
-import os
-import sys
-
-# Emit proper CGI header
-sys.stdout.write("Content-Type: text/plain\r\n\r\n")  # CRLF + CRLF
-
+sys.stdout.write("Content-Type: text/plain\r\n\r\n")
 print("Hello from CGI!")
 print(f"Method: {os.environ.get('REQUEST_METHOD')}")
 print(f"Query: {os.environ.get('QUERY_STRING')}")
-print(f"Content-Length: {os.environ.get('CONTENT_LENGTH')}")
-if os.environ.get("REQUEST_METHOD") == "POST":
-    print(f"Input body: {sys.stdin.read()}")
-else:
-    print("Input body: <not read on GET>")
--- cgi-bin/hello.sh
+++ cgi-bin/hello.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 echo -e "Content-Type: text/plain\r\n\r\n"
-echo
 echo "Hello from Bash CGI"
