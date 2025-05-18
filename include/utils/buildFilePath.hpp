930f017f249e53ee9cb459623806820e076b75aa
Rychkov Iurii <irychkov@student.hive.fi>
2025-05-18 16:27:36 +0300

    Add mkdirRecursive and upload.conf.

--- include/utils/buildFilePath.hpp
+++ include/utils/buildFilePath.hpp
@@ -6,7 +6,7 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 16:13:00 by irychkov          #+#    #+#             */
-/*   Updated: 2025/05/15 19:19:50 by irychkov         ###   ########.fr       */
+/*   Updated: 2025/05/18 12:38:47 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@
 #include "core/Server.hpp"
 #include "http/HttpRequest.hpp"
 #include <string>
+#include <cstring>
+#include <sstream>
+#include <sys/stat.h>
+#include <iostream>
 
 std::string joinPath(const std::string& base, const std::string& suffix);
 std::string buildFilePath(const HttpRequest& request, const Location& loc);
+bool mkdirRecursive(const std::string& path);
